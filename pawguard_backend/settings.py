48a3dddcc32695af4728b8db0ff882a8d3771c5b
Elynzx <rpascual06@outlook.com>
2026-08-11 17:38:54 -0500

    feat: add users, locations, plans, pets, and policies apps to INSTALLED_APPS

--- pawguard_backend/settings.py
+++ pawguard_backend/settings.py
@@ -22,7 +22,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "common",
+    "users",
+    "locations",
+    "plans",
+    "pets",
+    "policies",
 ]
+
+AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
