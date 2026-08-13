4490c94ded4961a8af7f3270351434253f3a917c
Elynzx <rpascual06@outlook.com>
2026-08-13 17:35:24 -0500

    fix: ensure password validation for user and superuser creation

--- users/managers.py
+++ users/managers.py
@@ -7,7 +7,12 @@ class UserManager(BaseUserManager):
             raise ValueError("El email es obligatorio.")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.set_password(password)
+
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
         user.save(using=self._db)
 
         return user
@@ -22,5 +27,8 @@ class UserManager(BaseUserManager):
 
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("El superusuario debe tener is_superuser=True.")
+
+        if not password:
+            raise ValueError("El superusuario debe tener una contraseña.")
 
         return self.create_user(email=email, password=password, **extra_fields)
