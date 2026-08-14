96e116ffa47806e44b1ce51b5950daa5c5701679
Elynzx <rpascual06@outlook.com>
2026-08-14 13:58:45 -0500

    feat: add routing for PetViewSet

---
+++ pets/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import PetViewSet
+
+router = DefaultRouter()
+router.register("", PetViewSet, basename="pet")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
