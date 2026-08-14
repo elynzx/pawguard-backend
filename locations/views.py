ce351ba4a094a69e8b917e052083553d7d94e5d1
Elynzx <rpascual06@outlook.com>
2026-08-14 12:31:41 -0500

    feat: implement DistrictViewSet and ClinicViewSet for handling District and Clinic data

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import Clinic, District
+from .serializers import ClinicSerializer, DistrictSerializer
+
+
+class DistrictViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = DistrictSerializer
+    queryset = District.objects.all()
+
+
+class ClinicViewSet(ReadOnlyModelViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = ClinicSerializer
+    queryset = Clinic.objects.all()
