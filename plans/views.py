c7c5da4016b75d2762c39c1f8d742e05863ed07f
Elynzx <rpascual06@outlook.com>
2026-08-14 12:46:51 -0500

    feat: implement PlanViewSet for read-only access to active plans

--- plans/views.py
+++ plans/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Plan
+from .serializers import PlanSerializer
+
+
+class PlanViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = PlanSerializer
+    queryset = Plan.objects.filter(is_active=True)
